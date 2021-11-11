0b6c31ef5031f12f68cfe3e54f67e0b802e40728
i201820189 <giomarlimocaldas1234@gmail.com>
2021-11-11 01:12:45 -0500

    cambios

--- src/App.js
+++ src/App.js
@@ -1,18 +1,59 @@
+import  { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Navigation  from './components/Navigation.js';
+import { todos } from './todos.json';
 
-function App() {
-  return (
-    <div className="App">
-      <header>
-        
-        <Navigation titulo="Mi primera Navegacion" />
-        <img src={logo} className="App-logo" alt="logo" />
+class App extends Component {
 
-      </header>
-    </div>
-  );
+  constructor() {
+    super();
+    this.state= {
+      todos
+    }
+  }
+
+  render(){
+    const todos = this.state.todos.map((todo, i) => {
+      return(
+        <div className="col-md-4">
+           <div className="card mt-4">
+              <div className="card-header">
+                <h3>{todo.title}</h3>
+                <span className="badge bg-danger ml-2">
+                  {todo.priority}
+                </span>
+              </div>
+              
+              <div className="card-body">
+                <p>{todo.description}</p>
+              </div>
+          </div>
+        </div>
+      )
+    })
+
+    return (
+      <div className="App">
+        <header>
+
+          <nav className="navbar navbar-dark bg-dark">
+            <a href="#" className="text-white">
+              Task
+            </a>
+          </nav>
+
+          <div className="container">
+            <div className="row mt-4">
+              { todos }
+            </div>
+          </div>
+
+          <img src={logo} className="App-logo" alt="logo" />
+  
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
